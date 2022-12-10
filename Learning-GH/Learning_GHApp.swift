1613eabdd0af94f29e949069ae5a1ff765812524
Matthew Simmons <matthewsimmons715@gmail.com>
2022-12-09 21:29:10 -0700

    Initial Commit

---
+++ Learning-GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning-GH
+//
+//  Created by Matthew Simmons on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
